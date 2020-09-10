6e034ecf1a0b65ed9afbb43a58e1d21e55297171
igorg <igorgonsalves96@hotmail.com>
2020-09-10 20:15:51 -0300

    Initial commit

---
+++ app/src/main/java/com/example/marvelopenapp/view/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.example.marvelopenapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.marvelopenapp.MainActivity
+import com.example.marvelopenapp.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        initHandler()
+    }
+
+    private fun initHandler() {
+        Handler().postDelayed({
+            startActivity(MainActivity.newIntent(this))
+        }, 5000)
+    }
+}
